b37a7fe618e71f975c80c808741f245258d2fe61
Radithya Ihza <ihzabowo96@gmail.com>
2024-06-17 02:47:23 +0700

    adding logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,11 +10,13 @@ export default async function Dashboard() {
   if (!session) {
     return redirect("/");
   }
+
   return (
     <div className="w-full sm:max-w-md px-5 justify-center">
       <h1 className="text-2xl mt-10 mb-6 font-bold">Dashboard</h1>
       <p className="bg-white p-5 text-justify">
-        Welcome to your dashboard! Explore and manage your account effortlessly.
+        Welcome {session.user.email} to your dashboard! Explore and manage your
+        account effortlessly.
       </p>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,54 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+const Logout = async () => {
+  "use server";
+
+  const supabase = createClient();
+  await supabase.auth.signOut();
+  redirect("/");
+};
+
 export default async function Header() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <nav className="flex justify-between items-center px-3 bg-white">
-        <div className="bg-cover bg-center w-32 h-20" style={{ backgroundImage: `url('https://s3-alpha-sig.figma.com/img/e65a/9814/8a3d1d87484fc8f49bfd5b7afb3f4ad6?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DNCFHHlWMhc50e1GemULx9hE7H38ygudI4OqyYGUdp5IeP-xHwnBJCwe7lQCwUA01rMVMGsj8-QYll9Dscz57RPE1N1nxsZBL92UsVpscvwuuryFROqbOQfxKksxk5HOs5agVcl1qLaE68gqn1DrlNKls733w8-tdBQub4x4Y-KIfGQRKIamca0BjBNOpdK-Hy~UkqF9qjUDdSMtycc95BcgcZRCEseAm-5M4zOgyUNejlYWnOt6cMC~ufE9xvmUJF37TpWq5nE8uI0VO44A-tdVSjKgawiD6DSwznQS849oYmdaSwXPZDTqt9UrmJ0yL6Rwm7uv1Oh1nLSlWOpBNA__')`}}></div>
+      <div
+        className="bg-cover bg-center w-32 h-20"
+        style={{
+          backgroundImage: `url('https://s3-alpha-sig.figma.com/img/e65a/9814/8a3d1d87484fc8f49bfd5b7afb3f4ad6?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DNCFHHlWMhc50e1GemULx9hE7H38ygudI4OqyYGUdp5IeP-xHwnBJCwe7lQCwUA01rMVMGsj8-QYll9Dscz57RPE1N1nxsZBL92UsVpscvwuuryFROqbOQfxKksxk5HOs5agVcl1qLaE68gqn1DrlNKls733w8-tdBQub4x4Y-KIfGQRKIamca0BjBNOpdK-Hy~UkqF9qjUDdSMtycc95BcgcZRCEseAm-5M4zOgyUNejlYWnOt6cMC~ufE9xvmUJF37TpWq5nE8uI0VO44A-tdVSjKgawiD6DSwznQS849oYmdaSwXPZDTqt9UrmJ0yL6Rwm7uv1Oh1nLSlWOpBNA__')`,
+        }}
+      ></div>
+
+      {!user ? (
         <div className="btns">
-            <button className='me-2 text-sm font-semibold leading-6 rounded-md py-2.5 px-6 bg-white hover:bg-slate-200'>Login</button>
-            <button className='rounded-md border border-indigo-500 bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Sign Up</button>
+          <a
+            href="/"
+            className="me-2 text-sm font-semibold leading-6 rounded-md py-2.5 px-6 bg-white hover:bg-slate-200"
+          >
+            Login
+          </a>
+          <a
+            href="/signup"
+            className="rounded-md border border-indigo-500 bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign Up
+          </a>
         </div>
+      ) : (
+        <form action={Logout} method="post" className="btns">
+          <button
+            type="submit"
+            className="me-2 text-sm font-semibold leading-6 rounded-md py-2.5 px-6 bg-black text-white hover:bg-slate-400"
+          >
+            Logout
+          </button>
+        </form>
+      )}
     </nav>
   );
 }
